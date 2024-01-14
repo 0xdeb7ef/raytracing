4d51c894c4606af9726355eadde05ef1fec65819
0xdeb7ef <45214659+0xdeb7ef@users.noreply.github.com>
2024-01-13 20:03:39 -0800

    refactor `Vector`

--- src/Vector.zig
+++ src/Vector.zig
@@ -3,16 +3,27 @@ const sqrt1_2 = @import("std").math.sqrt1_2;
 
 pub fn Vector(comptime size: usize, comptime T: type) type {
     return struct {
-        const Self = @Vector(size, T);
+        const Vec = @Vector(size, T);
 
-        pub inline fn init(data: anytype) Self {
+        pub inline fn init(data: anytype) Vec {
             switch (@TypeOf(data)) {
                 T, comptime_int, comptime_float => return @splat(data),
                 else => return data,
             }
         }
 
-        pub inline fn dot(u: Self, v: Self) T {
+        pub inline fn mag(u: Vec) f32 {
+            return @sqrt(mag_squared(u));
+        }
+        pub inline fn mag_squared(u: Vec) T {
+            return @reduce(.Add, (u * u));
+        }
+
+        pub inline fn unitVector(u: Vec) Vec {
+            return u / @as(Vec, @splat(mag(u)));
+        }
+
+        pub inline fn dot(u: Vec, v: Vec) T {
             var sum: T = 0;
             comptime var i: usize = 0;
             inline while (i < size) : (i += 1) {
@@ -21,18 +32,7 @@ pub fn Vector(comptime size: usize, comptime T: type) type {
             return sum;
         }
 
-        pub inline fn mag(u: Self) f32 {
-            return @sqrt(mag_squared(u));
-        }
-        inline fn mag_squared(u: Self) T {
-            return @reduce(.Add, (u * u));
-        }
-
-        pub inline fn unitVector(u: Self) Self {
-            return u / @as(Self, @splat(mag(u)));
-        }
-
-        pub inline fn cross(u: Self, v: Self) Self {
+        pub inline fn cross(u: Vec, v: Vec) Vec {
             comptime {
                 if (size != 3) @panic("cross product of vectors is only implemented for size 3 at the moment");
             }
@@ -67,19 +67,6 @@ test "init" {
     }
 }
 
-test "dot product" {
-    const Vec3 = Vector(3, f32);
-    const vec1 = Vec3.init(.{ 1, 2, 3 });
-    const vec2 = Vec3.init(.{ 4, -5, 6 });
-
-    try testing.expectEqual(12, Vec3.dot(vec1, vec2));
-
-    const vec3 = Vec3.init(.{ 6, -1, 3 });
-    const vec4 = Vec3.init(.{ 4, 18, -2 });
-
-    try testing.expectEqual(0, Vec3.dot(vec3, vec4));
-}
-
 test "magnitude of a vector" {
     const Vec2 = Vector(2, f32);
     const vec1 = Vec2.init(.{ 3, 4 });
@@ -95,6 +82,19 @@ test "unit vector" {
     inline while (i < 2) : (i += 1) {
         try testing.expectEqual(sqrt1_2, Vec2.unitVector(vec1)[i]);
     }
+}
+
+test "dot product" {
+    const Vec3 = Vector(3, f32);
+    const vec1 = Vec3.init(.{ 1, 2, 3 });
+    const vec2 = Vec3.init(.{ 4, -5, 6 });
+
+    try testing.expectEqual(12, Vec3.dot(vec1, vec2));
+
+    const vec3 = Vec3.init(.{ 6, -1, 3 });
+    const vec4 = Vec3.init(.{ 4, 18, -2 });
+
+    try testing.expectEqual(0, Vec3.dot(vec3, vec4));
 }
 
 test "cross product" {
